eb6bec43c818c403f2c3b82750900700e4e0b133
OstapZ <ostap@dev.sevenshift.de>
2024-12-08 13:27:47 +0200

    Add test gh workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,45 +4,46 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  workers: process.env.CI ? Number(process.env.CORES) / 2 || 1 : undefined,
+
+  reporter: [["html", { outputFolder: "playwright-report", open: "never" }]],
   use: {
-    trace: 'retain-on-failure',
+    trace: "retain-on-failure",
   },
   projects: [
     {
-      name: 'api',
+      name: "api",
       use: {
-        baseURL: process.env.API_BASE_URL || 'http://localhost:3000/api',
+        baseURL: process.env.API_BASE_URL || "http://localhost:3000/api",
       },
-      testDir: './tests/api',
+      testDir: "./tests/api",
     },
     {
-      name: 'e2e:chromium',
-      testDir: './tests/e2e',
+      name: "e2e:chromium",
+      testDir: "./tests/e2e",
       use: {
-        ...devices['Desktop Chrome'],
-        baseURL: process.env.UI_BASE_URL || 'http://localhost:3000',
-      },
-    },
-    {
-      name: 'e2e:firefox',
-      testDir: './tests/e2e',
-      use: {
-        ...devices['Desktop Firefox'],
-        baseURL: process.env.UI_BASE_URL || 'http://localhost:3000',
+        ...devices["Desktop Chrome"],
+        baseURL: process.env.UI_BASE_URL || "http://localhost:3000",
       },
     },
     {
-      name: 'e2e:webkit',
-      testDir: './tests/e2e',
+      name: "e2e:firefox",
+      testDir: "./tests/e2e",
       use: {
-        ...devices['Desktop Safari'],
-        baseURL: process.env.UI_BASE_URL || 'http://localhost:3000',
+        ...devices["Desktop Firefox"],
+        baseURL: process.env.UI_BASE_URL || "http://localhost:3000",
+      },
+    },
+    {
+      name: "e2e:webkit",
+      testDir: "./tests/e2e",
+      use: {
+        ...devices["Desktop Safari"],
+        baseURL: process.env.UI_BASE_URL || "http://localhost:3000",
       },
     },
   ],
--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,13 @@ import PlaywrightClient from "../api/clients/playwright.client";
 import ExamplePage from "../pages/example.page";
 
 class App {
-  public readonly examplePage = new ExamplePage(this.page);
-  public readonly api = new API(new PlaywrightClient(this.page.request));
+  public readonly examplePage: ExamplePage;
+  public readonly api: API;
 
-  public constructor(private readonly page: Page) {}
+  public constructor(private readonly page: Page) {
+    this.examplePage = new ExamplePage(this.page);
+    this.api = new API(new PlaywrightClient(this.page.request));
+  }
 }
 
 export default App;
